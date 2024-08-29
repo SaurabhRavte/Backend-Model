fc17456125d9f1765b5aa2553246e5ea145f1d34
Saurabh Ravte <saurabh.ravte@gmail.com>
2024-08-29 20:03:18 +0530

    define Schema

--- hospital-management/patient.models.js
+++ hospital-management/patient.models.js
@@ -1,5 +1,40 @@
 import mongoose from "mongoose";
 
-const patientSchema = new mongoose.Schema({}, { timestamps: true });
+const patientSchema = new mongoose.Schema(
+  {
+    name: {
+      string: {
+        type: String,
+        require: true,
+      },
+      diagnosedWith: {
+        type: String,
+        required: true,
+      },
+      address: {
+        type: String,
+        required: true,
+      },
+      age: {
+        type: Number,
+        required: true,
+      },
+      bloodGroup: {
+        type: String,
+        required: true,
+      },
+      gender: {
+        type: String,
+        required: true,
+        enum: ["M", "F", "Others"],
+      },
+      admitted: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Hospital",
+      },
+    },
+  },
+  { timestamps: true }
+);
 
 export const Patient = mongoose.model("Patient", patientSchema);
